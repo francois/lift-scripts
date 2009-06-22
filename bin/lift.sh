752b01179614f7b2a93244de95714418e661f67c
François Beausoleil <francois@teksol.info>
2009-06-22 11:11:35 -0400

    Begin handling multiple commands

--- bin/lift.sh
+++ bin/lift.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
-GROUP_ID=${1:?"GroupID must be specified (example: com.mycompany)"}
-ARTIFACT_ID=${2:?"ArtifactID must be specified (example: twitter).  This is the project's name."}
+case $1 in
+  gen|generate)
+    GROUP_ID=${2:?"GroupID must be specified (example: com.mycompany)"}
+    ARTIFACT_ID=${3:?"ArtifactID must be specified (example: twitter).  This is the project's name."}
 
-mvn archetype:generate -U                                   \
-  -DarchetypeGroupId=net.liftweb                            \
-  -DarchetypeArtifactId=lift-archetype-basic                \
-  -DarchetypeVersion=1.0                                    \
-  -DremoteRepositories=http://scala-tools.org/repo-releases \
-  -DgroupId=$GROUP_ID                                       \
-  -DartifactId=$ARTIFACT_ID                                 \
-  -Dversion=1.0-SNAPSHOT
+    mvn archetype:generate -U                                   \
+      -DarchetypeGroupId=net.liftweb                            \
+      -DarchetypeArtifactId=lift-archetype-basic                \
+      -DarchetypeVersion=1.0                                    \
+      -DremoteRepositories=http://scala-tools.org/repo-releases \
+      -DgroupId=$GROUP_ID                                       \
+      -DartifactId=$ARTIFACT_ID                                 \
+      -Dversion=1.0-SNAPSHOT
+    ;;
+
+  *)
+    echo "Unrecognized command: $1"
+    echo "Valid commands are: gen|generate"
+    exit 1
+esac
