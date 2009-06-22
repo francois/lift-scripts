023c571bff0860e8000f0d10cfedbd272a805ba5
François Beausoleil <francois@teksol.info>
2009-06-22 11:17:07 -0400

    Added start, test and help commands.

--- bin/lift.sh
+++ bin/lift.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-case $1 in
+COMMAND=${1:?"Command must be specified.  Try '$0 help'."}
+
+case $COMMAND in
   gen|generate)
     GROUP_ID=${2:?"GroupID must be specified (example: com.mycompany)"}
     ARTIFACT_ID=${3:?"ArtifactID must be specified (example: twitter).  This is the project's name."}
@@ -12,10 +14,20 @@ case $1 in
       -DgroupId=$GROUP_ID                                       \
       -DartifactId=$ARTIFACT_ID                                 \
       -Dversion=1.0-SNAPSHOT
-    ;;
 
-  *)
+    echo "Project $ARTIFACT_ID generated.  Start the server with '$0 start' from $ARTIFACT_ID/"
+  ;;
+
+  test)
+    mvn test
+  ;;
+
+  start)
+    mvn jetty:run
+  ;;
+
+  help|*)
     echo "Unrecognized command: $1"
-    echo "Valid commands are: gen|generate"
+    echo "Valid commands are: gen|generate, test and start"
     exit 1
 esac
